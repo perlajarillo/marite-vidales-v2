e559feef4c0fa871bfeea5eff5ffa3ba2b573d02
Perla Jarillo <perlai.jarillo@gmail.com>
2026-09-09 17:29:42 -0600

    fix: active index in sidebar menu, exhibits carousel size

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,9 +8,29 @@ import {
   FacebookIcon,
   InstagramIcon,
 } from "../SocialMedia/SocialMedia";
+import { useLocation } from "react-router";
+
+const getActiveIndexFromPath = (currentUrl: string): number => {
+  if (currentUrl.includes("series")) {
+    return 1;
+  } else if (currentUrl.includes("exhibits")) {
+    return 2;
+  } else if (currentUrl.includes("biography")) {
+    return 3;
+  } else if (currentUrl.includes("reviews")) {
+    return 4;
+  } else if (currentUrl.includes("contact")) {
+    return 5;
+  } else {
+    return 0;
+  }
+};
 
 const Sidebar: React.FC = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const location = useLocation();
+  const [activeIndex, setActiveIndex] = useState(
+    getActiveIndexFromPath(location.pathname),
+  );
 
   const year = new Date().getFullYear();
 
@@ -22,6 +42,7 @@ const Sidebar: React.FC = () => {
     { label: intl.reviews, to: "/reviews" },
     { label: intl.contact, to: "/contact" },
   ];
+
   return (
     <aside className={styles.sidebar}>
       <div className={styles.sidebarHeader}>
